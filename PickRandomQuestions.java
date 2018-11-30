88af9eef2962ca190c5045c745123b943c66fd5e
zb2009 <zb55427@gmail.com>
2018-11-30 11:01:39 -0600

    Update and rename Test.java to PickRandomQuestions.java

---
+++ PickRandomQuestions.java
@@ -0,0 +1,42 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.ArrayList;
+public interface PickRandomQuestions
+{
+    ConcurrentLinkedQueue<ArrayList<Question>> que = new ConcurrentLinkedQueue<ArrayList<Question>>();
+    
+    // Starts loading the questions.
+    default void startRandom(){
+        LoadQuestions ques = new LoadQuestions(que);
+        Thread t = new Thread(ques);
+        t.start();
+    }
+    
+    //Returns an array of 3 random questions from a section.
+    default Question[] pickRandom(){
+        int count = 0;
+        ArrayList<Question> alist = null;
+        Question[] questions = null;
+        while(count<5){
+            alist = que.poll();
+            if(alist==null){
+                System.out.println("READING WAITING!!!");
+                count++;
+                try{
+                    Thread.sleep(200);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                count = 0;
+                questions = new Question[3];
+                for(int i=0;i<3;i++){
+                    int ran = (int)(Math.random()*alist.size());
+                    questions[i] = alist.remove(ran);
+                }
+                return questions;
+            }
+
+        }
+        return questions;
+    }
+}
