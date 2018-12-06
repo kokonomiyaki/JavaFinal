8b63bf3434b24c82f62e51994ff80d81fba79fa5
zb2009 <zb55427@gmail.com>
2018-12-05 21:29:57 -0600

    Update TestFrame.java

--- TestFrame.java
+++ TestFrame.java
@@ -4,16 +4,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.event.WindowEvent;
 
 public class TestFrame extends JFrame implements PickRandomQuestions, ActionListener{
     JPanel ansPanel = new JPanel();
     JLabel questionLabel = new JLabel("Click Start to begin test.", SwingConstants.CENTER);
     JButton startButton = new JButton("Start");
     JButton submit = new JButton("Submit");
+    JButton retry = new JButton("Retry");
+    JButton quit = new JButton("Quit");
     Container cp = getContentPane();
 
     List<Question> questions = new ArrayList<>();
-
+    boolean[] answersCorrect = new boolean[30];
     int count = 0;
 
     List<String> radioStringList = new ArrayList<>();
@@ -51,52 +54,77 @@ public class TestFrame extends JFrame implements PickRandomQuestions, ActionList
 
         count++;
     }
+    public void setRetry(){
+        cp.remove(submit);
+        JPanel tmp = new JPanel();
+        tmp.add(retry);
+        tmp.add(quit);
+        cp.add(tmp,BorderLayout.SOUTH);
+        quit.addActionListener(this);
+        retry.addActionListener(this);
+        TestFrame.this.pack();
+        this.setSize(600,400);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e){
         Object source = e.getSource();
 
         if(source == startButton) {
-             startRandom();
-             Question[] questions1 = pickRandom();
-             questionLabel.setText("<html>" + questions1[count].getQuestion() + "</html>");
+            startRandom();
+            Question[] questions1 = pickRandom();
+            questionLabel.setText("<html>" + questions1[count].getQuestion() + "</html>");
 
+            while(questions1!=null){
+                for(Question q : questions1){
+                    questions.add(q);
+                    System.out.println(q.getQuestion());
+                    System.out.println(q.getAnswers());
+                }
+                //System.out.println(++count);
+                questions1 = pickRandom();
+            }
+            setRadioButtons();
 
-             while(questions1!=null){
-             for(Question q : questions1){
-                 questions.add(q);
-             System.out.println(q.getQuestion());
-             System.out.println(q.getAnswers());
-             }
-             //System.out.println(++count);
-             questions1 = pickRandom();
-             }
-             setRadioButtons();
-
-             cp.remove(startButton);
-             cp.add(submit, BorderLayout.SOUTH);
-             cp.add(ansPanel, BorderLayout.CENTER);
-             submit.addActionListener(this);
+            cp.remove(startButton);
+            cp.add(submit, BorderLayout.SOUTH);
+            cp.add(ansPanel, BorderLayout.CENTER);
+            submit.addActionListener(this);
 
         }
 
         if(source == submit){
 
 
-
             for(int i = 0; i < radioButtonList.size(); i++){
-            if (radioButtonList.get(i).isSelected()){
-                questions.get(i).isCorrect(i);
-                System.out.println(questions.get(count).isCorrect(i));
+                if (radioButtonList.get(i).isSelected()){
+                    answersCorrect[count] = questions.get(i).isCorrect(i);
+                    System.out.println(questions.get(count).isCorrect(i));
+                }
             }
+
+            if(count==2){
+                setRetry();
+                System.out.println("ending");
+            } else {
+                questionLabel.setText("<html>" + questions.get(count).getQuestion() + "</html>");
+
+                ansPanel.repaint();
+
+                setRadioButtons();
             }
-
-
-            questionLabel.setText("<html>" + questions.get(count).getQuestion() + "</html>");
-
-            ansPanel.repaint();
-
-            setRadioButtons();
+            cp.repaint();
+        }
+        if(source == retry){
+            
+            this.setVisible(false);
+            new Test();
+            
+        }
+        if(source == quit){
+            
+            this.dispatchEvent(new WindowEvent(this,WindowEvent.WINDOW_CLOSING));
         }
     }
+   
 }
