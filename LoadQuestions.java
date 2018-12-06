c57e8526123bf49566757798d687ccaaae3aac26
zb2009 <zb55427@gmail.com>
2018-12-06 00:46:53 -0600

    Final Update (#12)
    Everything is working, there is a jar file with the code in it, a zip that contains the jar and txt files. Since he moved the deadline back and in the assignment_final file he says all the things needed to be handed in contains our test scores, I'm not turning mine in till we take the final in class.?

--- LoadQuestions.java
+++ LoadQuestions.java
@@ -4,54 +4,56 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.io.File;
 import java.io.IOException;
 
-public class LoadQuestions implements Runnable
-{
-    private ConcurrentLinkedQueue<ArrayList<Question>> que;
-    //@Param1 The queue that you will be pulling questions from.
-    public LoadQuestions(ConcurrentLinkedQueue<ArrayList<Question>> que){
-        this.que = que;
-    }
+public class LoadQuestions implements Runnable {
+	private ConcurrentLinkedQueue<ArrayList<Question>> que;
 
-    public void run(){
-        Scanner fileReader;
-        //using FileIO and Exception to load questionBank.txt file  (Thau)
-        try{
-            fileReader = new Scanner(new File("questionBank.txt"));
-        } catch(IOException e){
-            System.out.println("File not found.");
-            return;
-        }
-        String holder;
-        ArrayList<Question> list = null;
+	// @Param1 The queue that you will be pulling questions from.
+	public LoadQuestions(ConcurrentLinkedQueue<ArrayList<Question>> que) {
+		this.que = que;
+	}
 
-        //loading each line of questionBank.txt file  (Thau)
+	public void run() {
+		Scanner fileReader;
+		// using FileIO and Exception to load questionBank.txt file (Thau)
+		try {
+			fileReader = new Scanner(new File("questionBank.txt"));
+		} catch (IOException e) {
+			System.out.println("File not found.");
+			return;
+		}
+		String holder;
+		ArrayList<Question> list = null;
 
-        while(fileReader.hasNext()){
-            holder = fileReader.nextLine();
-            //slipt 100 quesions into 10 different topics using charAt() to check if the first character of the line is ":"  (Thau)
-            if(holder.charAt(0)==':' || holder.charAt(1)==':'){
-            	// Checks to make sure its not on the first rotation before offering the packet of questions.
-                if(list!=null){
-                    que.offer(list);
-                }
-                list = new ArrayList<Question>();//add to the Question Arraylist  (Thau)
+		// loading each line of questionBank.txt file (Thau)
 
-                fileReader.nextLine();
-            } else {
-            	// Parsing a question.
-                String question = holder;
-                int answerIndex = Integer.parseInt(fileReader.nextLine());
-                holder = fileReader.nextLine();
-                ArrayList<String> answers = new ArrayList<String>();
-                while(!holder.isEmpty()){
-                    answers.add(holder);
-                    holder = fileReader.nextLine();
-                }
-                list.add(new Question(question,answers,answerIndex));
-            } 
-        }
-        que.offer(list);
-        fileReader.close();
-        return;
-    }
+		while (fileReader.hasNext()) {
+			holder = fileReader.nextLine();
+			// slipt 100 quesions into 10 different topics using charAt() to check if the
+			// first character of the line is ":" (Thau)
+			if (holder.charAt(0) == ':' || holder.charAt(1) == ':') {
+				// Checks to make sure its not on the first rotation before offering the packet
+				// of questions.
+				if (list != null) {
+					que.offer(list);
+				}
+				list = new ArrayList<Question>();// add to the Question Arraylist (Thau)
+
+				fileReader.nextLine();
+			} else {
+				// Parsing a question.
+				String question = holder;
+				int answerIndex = Integer.parseInt(fileReader.nextLine());
+				holder = fileReader.nextLine();
+				ArrayList<String> answers = new ArrayList<String>();
+				while (!holder.isEmpty()) {
+					answers.add(holder);
+					holder = fileReader.nextLine();
+				}
+				list.add(new Question(question, answers, answerIndex));
+			}
+		}
+		que.offer(list);
+		fileReader.close();
+		return;
+	}
 }
--- TestFrame.java
+++ TestFrame.java
@@ -3,128 +3,178 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
 
-public class TestFrame extends JFrame implements PickRandomQuestions, ActionListener{
-    JPanel ansPanel = new JPanel();
-    JLabel questionLabel = new JLabel("Click Start to begin test.", SwingConstants.CENTER);
-    JButton startButton = new JButton("Start");
-    JButton submit = new JButton("Submit");
-    JButton retry = new JButton("Retry");
-    JButton quit = new JButton("Quit");
-    Container cp = getContentPane();
+public class TestFrame extends JFrame implements PickRandomQuestions, ActionListener {
+	JPanel ansPanel = new JPanel();
+	JLabel questionLabel = new JLabel("Click Start to begin test.", SwingConstants.CENTER);
+	JButton startButton = new JButton("Start");
+	JButton submit = new JButton("Submit");
+	JButton retry = new JButton("Retry");
+	JButton quit = new JButton("Quit");
+	JTextField nameInput = new JTextField(30);
+	ChartPanel graph;
+	Container cp = getContentPane();
+	String[] catagories = { "Arrays", "Inheritance", "Interface", "Software Dev", "Regex", "File I/O", "Lambdas",
+			"Generics", "Recursion", "Concurrency" };
 
-    List<Question> questions = new ArrayList<>();
-    boolean[] answersCorrect = new boolean[30];
-    int count = 0;
+	List<Question> questions = new ArrayList<>();
+	boolean[] answersCorrect = new boolean[30];
+	int count = 0;
 
-    List<String> radioStringList = new ArrayList<>();
-    List<JRadioButton> radioButtonList = new ArrayList<>();
+	List<String> radioStringList = new ArrayList<>();
+	List<JRadioButton> radioButtonList = new ArrayList<>();
 
-    public TestFrame(String title){
-        super(title);
+	public TestFrame(String title) {
+		super(title);
 
-        setLayout(new BorderLayout());
+		setLayout(new BorderLayout());
 
-        ansPanel.setLayout(new BoxLayout(ansPanel, BoxLayout.PAGE_AXIS));
+		ansPanel.setLayout(new BoxLayout(ansPanel, BoxLayout.PAGE_AXIS));
 
-        cp.add(questionLabel, BorderLayout.NORTH);
-        cp.add(startButton, BorderLayout.SOUTH);
+		cp.add(questionLabel, BorderLayout.NORTH);
+		cp.add(startButton, BorderLayout.SOUTH);
 
-        startButton.addActionListener(this);
+		startButton.addActionListener(this);
 
-    }
+	}
 
-    public void setRadioButtons(){
-        //Pretty self explanatory, sets up radio buttons
-        ButtonGroup radioGroup = new ButtonGroup();
+	public void setRadioButtons() {
+		// Pretty self explanatory, sets up radio buttons
+		ButtonGroup radioGroup = new ButtonGroup();
 
-        radioStringList.clear();
-        radioButtonList.clear();
-        ansPanel.removeAll();
+		radioStringList.clear();
+		radioButtonList.clear();
+		ansPanel.removeAll();
 
-        for(int i = 0; i < questions.get(count).getAnswers().size(); i++) {
-            radioStringList.add("" + questions.get(count).getAnswers().get(i));
-            JRadioButton answerButton = new JRadioButton(radioStringList.get(i));
-            radioButtonList.add(answerButton);
-            radioGroup.add(radioButtonList.get(i));
-            ansPanel.add(radioButtonList.get(i));
-        }
+		System.out.println(questions.get(count).getQuestion());
+		for (int i = 0; i < questions.get(count).getAnswers().size(); i++) {
+			radioStringList.add("" + questions.get(count).getAnswers().get(i));
+			JRadioButton answerButton = new JRadioButton(radioStringList.get(i));
+			radioButtonList.add(answerButton);
+			radioGroup.add(radioButtonList.get(i));
+			ansPanel.add(radioButtonList.get(i));
+		}
 
-        count++;
-    }
-    public void setRetry(){
-        cp.remove(submit);
-        JPanel tmp = new JPanel();
-        tmp.add(retry);
-        tmp.add(quit);
-        cp.add(tmp,BorderLayout.SOUTH);
-        quit.addActionListener(this);
-        retry.addActionListener(this);
-        TestFrame.this.pack();
-        this.setSize(600,400);
-    }
+		count++;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e){
-        Object source = e.getSource();
+	public void setRetry() {
+		cp.removeAll();
+		JPanel retryPanel = new JPanel();
+		retryPanel.add(retry);
+		retryPanel.add(quit);
+		cp.add(retryPanel, BorderLayout.SOUTH);
+		quit.addActionListener(this);
+		retry.addActionListener(this);
+		retryPanel.setVisible(true);
+		JPanel namePanel = new JPanel();
+		namePanel.add(new JLabel("Put your name here:"));
+		namePanel.add(nameInput);
+		cp.add(namePanel, BorderLayout.CENTER);
+		this.setSize(601, 400);
+		this.setSize(600, 400);
+	}
 
-        if(source == startButton) {
-            startRandom();
-            Question[] questions1 = pickRandom();
-            questionLabel.setText("<html>" + questions1[count].getQuestion() + "</html>");
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
 
-            while(questions1!=null){
-                for(Question q : questions1){
-                    questions.add(q);
-                    System.out.println(q.getQuestion());
-                    System.out.println(q.getAnswers());
-                }
-                //System.out.println(++count);
-                questions1 = pickRandom();
-            }
-            setRadioButtons();
+		if (source == startButton) {
+			startRandom();
+			Question[] questions1 = pickRandom();
+			questionLabel.setText("<html>" + questions1[count].getQuestion() + "</html>");
 
-            cp.remove(startButton);
-            cp.add(submit, BorderLayout.SOUTH);
-            cp.add(ansPanel, BorderLayout.CENTER);
-            submit.addActionListener(this);
+			while (questions1 != null) {
+				for (Question q : questions1) {
+					questions.add(q);
+				}
+				questions1 = pickRandom();
+			}
+			setRadioButtons();
 
-        }
+			cp.remove(startButton);
+			cp.add(submit, BorderLayout.SOUTH);
+			cp.add(ansPanel, BorderLayout.CENTER);
+			submit.addActionListener(this);
 
-        if(source == submit){
+		}
 
+		if (source == submit) {
+			for (int i = 0; i < radioButtonList.size(); i++) {
+				if (radioButtonList.get(i).isSelected()) {
+					answersCorrect[count - 1] = questions.get(count - 1).isCorrect(i);
+					System.out.println((questions.get(count - 1).isCorrect(i) ? "Correct" : "Incorrect"));
+					System.out.println(questions.get(count - 1).correctAnswer());
+				}
 
-            for(int i = 0; i < radioButtonList.size(); i++){
-                if (radioButtonList.get(i).isSelected()){
-                    answersCorrect[count] = questions.get(i).isCorrect(i);
-                    System.out.println(questions.get(count).isCorrect(i));
-                }
-            }
+			}
+			if (count == 30) {
+				setRetry();
+				int[] correct = new int[10];
+				for (int i = 0; i < answersCorrect.length; i++) {
+					if (answersCorrect[i]) {
+						correct[(i) / 3]++;
+						System.out.print(i + " ");
+					}
+				}
+				graph = new ChartPanel(correct);
+			} else {
 
-            if(count==2){
-                setRetry();
-                System.out.println("ending");
-            } else {
-                questionLabel.setText("<html>" + questions.get(count).getQuestion() + "</html>");
+				questionLabel.setText("<html>" + questions.get(count).getQuestion() + "</html>");
 
-                ansPanel.repaint();
+				ansPanel.repaint();
 
-                setRadioButtons();
-            }
-            cp.repaint();
-        }
-        if(source == retry){
-            
-            this.setVisible(false);
-            new Test();
-            
-        }
-        if(source == quit){
-            
-            this.dispatchEvent(new WindowEvent(this,WindowEvent.WINDOW_CLOSING));
-        }
-    }
-   
+				setRadioButtons();
+			}
+			cp.repaint();
+		}
+		if (source == retry) {
+			saveTest();
+
+			this.setVisible(false);
+			graph.setVisible(false);
+
+			new Test();
+
+		}
+		if (source == quit) {
+			saveTest();
+			System.exit(0);
+		}
+	}
+
+	public void saveTest() {
+		SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyyHHmmss");
+		Date date = new Date();
+		String fileName = nameInput.getText() + "_" + formatter.format(date) + ".txt";
+		fileName = fileName.replaceAll(" ", "");
+		try {
+			PrintWriter writer = new PrintWriter(new File(fileName));
+			for (int i = 0; i < 10; i++) {
+				writer.println(catagories[i]);
+				for (int j = 0; j < 3; j++) {
+					writer.println((i * 3) + j + 1 + ") " + ((answersCorrect[(i * 3) + j]) ? "Correct" : "Incorrect"));
+				}
+			}
+			int totalCorrect = 0;
+			for (boolean i : answersCorrect) {
+				if (i) {
+					totalCorrect++;
+				}
+			}
+
+			double correctPercentage = ((double) totalCorrect / 30) * 100;
+			writer.printf("Percent of correct answers: %.2f", correctPercentage);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
